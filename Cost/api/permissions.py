2e2a5474289b6df4149e26e67b827e1a2384902c
Дмитрий Петухов <dapetuhov@yandex.ru>
2022-06-17 22:04:44 +0500

    начало проекта положено!

--- Cost/api/permissions.py
+++ Cost/api/permissions.py
@@ -3,5 +3,7 @@ from rest_framework import permissions
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return True if request.method == permissions.SAFE_METHODS \
-            else obj.author == request.user
+        return obj.author == request.user
+
+    def has_permission(self, request, view, obj):
+        return obj.author == request.user
