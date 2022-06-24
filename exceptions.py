d8fdea6ae3c686cfb2f1e21e5e8cea90c5bf3351
Dmitry-russ <dapetuhov@yandex.ru>
2022-06-24 23:13:46 +0500

    удалил неиспользуемое

--- exceptions.py
+++
@@ -1,8 +0,0 @@
-class SendMessageError(Exception):
-    """Класс исключения при ошибке отправки сообщения."""
-
-    def __init__(self, id):
-        self.id = id
-
-    def __str__(self):
-        return f'Telegram can not send message to: {self.id}.'
