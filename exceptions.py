0b58d7a0dc0d0e7fa7740ddfd7eef7c2a7d381b7
Dmitry-russ <dapetuhov@yandex.ru>
2022-06-24 20:35:53 +0500

    убрал лишнее исключение

--- exceptions.py
+++ exceptions.py
@@ -6,13 +6,3 @@ class SendMessageError(Exception):
 
     def __str__(self):
         return f'Telegram can not send message to: {self.id}.'
-
-
-class NotIndexError(Exception):
-    """Класс исключения при отсутствии ключа в словаре."""
-
-    def __init__(self, keys):
-        self.keys = keys
-
-    def __str__(self):
-        return f'Can not find keys: {self.keys}.'
