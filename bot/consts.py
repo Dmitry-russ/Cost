28597c5a7b9aae7f6a541b801b539b3b8b0dcc2f
Dmitry-russ <dapetuhov@yandex.ru>
2022-12-07 21:57:29 +0500

    поправил для длокальной работы

--- bot/consts.py
+++ bot/consts.py
@@ -1,5 +1,11 @@
-ENDPOINT: str = 'http://dmitrypetukhov90.pythonanywhere.com/api/v1/costs/'
+# ENDPOINT: str = 'http://dmitrypetukhov90.pythonanywhere.com/api/v1/costs/'
+# GROUP_ENDPOINT: str = (
+#     'http://dmitrypetukhov90.pythonanywhere.com/api/v1/groups/')
+# USER_ENDPOINT: str = (
+#     'http://dmitrypetukhov90.pythonanywhere.com/auth/jwt/create/')
+
+ENDPOINT: str = 'http://127.0.0.1:8000/api/v1/costs/'
 GROUP_ENDPOINT: str = (
-    'http://dmitrypetukhov90.pythonanywhere.com/api/v1/groups/')
+    'http://127.0.0.1:8000/api/v1/groups/')
 USER_ENDPOINT: str = (
-    'http://dmitrypetukhov90.pythonanywhere.com/auth/jwt/create/')
+    'http://127.0.0.1:8000/auth/jwt/create/')
